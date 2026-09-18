a890fa261f7b999d6b73435b3a38fe4ff6cd95af
kishorkhatiwada9-dev <kishorkhatiwada9@gmail.com>
2026-09-19 00:41:51 +0545

    sell function is added but still some logic needs to be updated in the same function.

--- inventory.py
+++ inventory.py
@@ -17,4 +17,46 @@ def main_menu():
     print(f"{'2. Sell Medicine'}")
     print(f"{'3. Restock Medicine'}")
     print(f"{'4. Save & Exit'}")
-  
+
+def sell_medicine(data):
+    
+    customer_name = input("Enter the name of customer: ").strip()
+    med_name = input("Enter medicine name or ('done' to exit): ").strip()
+
+    med_1 = data[0].split(",")
+
+
+    #Accessing the data of Paracetamol
+    Paracetamol_med = med_1[0].strip()
+    Lomus = med_1[1].strip()
+    qty_paracetamol = int(med_1[2].strip())
+    price_tab = med_1[3].strip()
+    price_strip = med_1[4].strip()
+    tab_strip = med_1[5].strip()
+
+    if med_name == "P":
+
+        # try:
+        tab_or_strip = input("You want to buy a 'tablet' or 'strip': ").strip()
+
+        if tab_or_strip.lower() == "tablet":
+            qty_tablet = int(input("Enter the quantyty: "))
+            if qty_tablet >=0 and qty_tablet <= 1200:
+                qty_paracetamol-=qty_tablet
+
+                med_1[2] = str(qty_paracetamol)
+                data[0] = ", ".join(med_1)
+
+
+        return data
+
+        
+
+
+    
+
+
+    
+    
+
+    
--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 
 from inventory import(show_medicine,
                             default_medicine,
-                            main_menu)
+                            main_menu,
+                            sell_medicine)
 
 
 
 exit = False
+
+# meds_1 = "inventory.txt"
+# print(meds_1)
 
 while exit == False:
 
     main_menu()
     
     input_1 = int(input("Enter a number: "))
+
     if input_1 == 1:
         meds = show_medicine(default_medicine)
         for i in meds:
             print(i)
-        exit = True
+        
+
+    elif input_1 == 2:
+        meds = sell_medicine(default_medicine)
+        
+        
+        
         
